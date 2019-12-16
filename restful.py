7b32f98fa35bb7b23b1693aa706baebebfd71936
Fabio Mora <andunker@hotmail.com>
2019-12-15 22:57:48 -0500

    argparse completed

--- restful.py
+++ restful.py
@@ -1,9 +1,16 @@
 #!./vEnv/bin/python
 
+import requests
 import argparse
+
 parser = argparse.ArgumentParser()
+parser.add_argument('METHOD' ,choices=['get', 'post'], help='Request method')
+parser.add_argument('endpoint', help='Request endpoint URI fragment')
+
+parser.add_argument('-d', '--data', help='Data to send with request')
+parser.add_argument('-o', '--output', help='Output to .json or .csv file (default: dump to stdout)')
+                
 parser.parse_args()
 
-
-print('hola')
+print('hello world')
 
