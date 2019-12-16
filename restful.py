f3170fe5c8ee34596c76b1d18b329716eccdd2d6
Fabio Mora <andunker@hotmail.com>
2019-12-16 00:06:37 -0500

    first get and exceptions

--- restful.py
+++ restful.py
@@ -1,16 +1,26 @@
 #!./vEnv/bin/python
 
+import json
+
 import requests
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('METHOD' ,choices=['get', 'post'], help='Request method')
-parser.add_argument('endpoint', help='Request endpoint URI fragment')
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('METHOD', choices=[
+                        'get', 'post'], help='Request method')
+    parser.add_argument('endpoint', help='Request endpoint URI fragment')
 
-parser.add_argument('-d', '--data', help='Data to send with request')
-parser.add_argument('-o', '--output', help='Output to .json or .csv file (default: dump to stdout)')
-                
-parser.parse_args()
+    parser.add_argument('-d', '--data', help='Data to send with request')
+    parser.add_argument(
+        '-o', '--output', help='Output to .json or .csv file (default: dump to stdout)')
 
-print('hello world')
+    arguments = parser.parse_args()
 
+    if(arguments.METHOD == 'get'):
+        response = requests.get(
+            'https://jsonplaceholder.typicode.com%s' % arguments.endpoint)
+
+    print(response.text)
+except Exception as exception:
+    print('Error')
